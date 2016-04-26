ad1cd36353dec9d47de7a9d6f784066edfc20d93
alex myronov <myronov.alexander@gmail.com>
2016-04-26 17:25:03 +0300

    added another version of Quick Sort implementation, by using native features of JS

--- src/index.js
+++ src/index.js
@@ -3,24 +3,13 @@ if(module.hot) {
   module.hot.accept();
 }
 
-import {List, Node} from './single-linked-list';
-import {DoublyList, DoublyNode} from './doubly-linked-list';
-import insertionSort from './insertion-sort';
-import binarySearch from './binary-search';
 
-// const t = new List();
 
-// t.add('hello');
-// t.add('world');
-
-// console.dir(t);
-
-// var sortedItems = insertionSort([1, -1, 0, 10, 4]);
-// console.log(sortedItems);
-
-import mergeSort from './merge-sort';
+import quickSort from './quick-sort-functional';
 var items = ["y", "z", "r","n","a","d","u","f","x","h","s","j", "i", "g"];
-console.log(mergeSort(items));
+
+var sorted = quickSort(items);
+console.log(sorted);
 
 // var items = ["a","b","c","d","e","f","g","h","i","j"];
 // console.log(binarySearch(items, "i"));
---
+++ src/quick-sort-functional.js
@@ -0,0 +1,22 @@
+
+export default function quickSort(arr) {
+  if (!arr.length) {
+    return arr;
+  }
+
+  const pivot = arr[0];
+  let left = [];
+  let right = [];
+
+  for (var i = 1; i < arr.length; i++) {
+
+    if (arr[i] < pivot) {
+      left.push(arr[i]);
+    } else {
+      right.push(arr[i]);
+    }
+
+  }
+
+  return quickSort(left).concat(pivot, quickSort(right));
+}
